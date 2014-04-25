450dc46febfd5059eb9c5fb36871913224b87d0f
root <root@101.226.10.8>
2014-04-25 09:29:13 -0500

    test main.

--- SimpleSpring/src/main/java/com/slieer/spring/scheduling/Application.java
+++ SimpleSpring/src/main/java/com/slieer/spring/scheduling/Application.java
@@ -1,10 +1,5 @@
 package com.slieer.spring.scheduling;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 
 public class Application {
@@ -12,10 +7,5 @@ public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ScheduledTasks.class);
         
-        "".substring(1, 3);
-        
-        Arrays.asList(new int[]{1,2});
-        
-        List<String> settings = Collections.nCopies(5, "Baby");  
     }
 }
