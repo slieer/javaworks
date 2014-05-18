88e6a5b9cada5864a6acf6a377ac93e0ab77d560
slieer <slieer@101.226.10.8>
2014-05-18 10:06:34 -0500

    add apache mahout 0.9 exam.

--- com.slieer.mahout.demo/src/main/java/com/slieer/mahout/RecommenderResult.java
+++ com.slieer.mahout.demo/src/main/java/com/slieer/mahout/RecommenderResult.java
@@ -11,28 +11,31 @@ import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 
 public class RecommenderResult {
 
-    final static int NEIGHBORHOOD_NUM = 2;
-    final static int RECOMMENDER_NUM = 3;
+	final static int NEIGHBORHOOD_NUM = 2;
+	final static int RECOMMENDER_NUM = 3;
 
-    public static void main(String[] args) throws TasteException, IOException {
-        String file = "datafile/job/pv.csv";
-        DataModel dataModel = RecommendFactory.buildDataModelNoPref(file);
-        RecommenderBuilder rb1 = RecommenderEvaluator.userCityBlock(dataModel);
-        RecommenderBuilder rb2 = RecommenderEvaluator.itemLoglikelihood(dataModel);
+	public static void main(String[] args) throws TasteException, IOException {
+		String file = "datafile/job/pv.csv";
+		DataModel dataModel = RecommendFactory.buildDataModelNoPref(file);
+		RecommenderBuilder rb1 = RecommenderEvaluator.userCityBlock(dataModel);
+		RecommenderBuilder rb2 = RecommenderEvaluator
+				.itemLoglikelihood(dataModel);
 
-        LongPrimitiveIterator iter = dataModel.getUserIDs();
-        while (iter.hasNext()) {
-            long uid = iter.nextLong();
-            System.out.print("userCityBlock    =>");
-            result(uid, rb1, dataModel);
-            System.out.print("itemLoglikelihood=>");
-            result(uid, rb2, dataModel);
-        }
-    }
+		LongPrimitiveIterator iter = dataModel.getUserIDs();
+		while (iter.hasNext()) {
+			long uid = iter.nextLong();
+			System.out.print("userCityBlock    =>");
+			result(uid, rb1, dataModel);
+			System.out.print("itemLoglikelihood=>");
+			result(uid, rb2, dataModel);
+		}
+	}
 
-    public static void result(long uid, RecommenderBuilder recommenderBuilder, DataModel dataModel) throws TasteException {
-        List<RecommendedItem> list = recommenderBuilder.buildRecommender(dataModel).recommend(uid, RECOMMENDER_NUM);
-        RecommendFactory.showItems(uid, list, false);
-    }
+	public static void result(long uid, RecommenderBuilder recommenderBuilder,
+			DataModel dataModel) throws TasteException {
+		List<RecommendedItem> list = recommenderBuilder.buildRecommender(
+				dataModel).recommend(uid, RECOMMENDER_NUM);
+		RecommendFactory.showItems(uid, list, false);
+	}
 
 }
